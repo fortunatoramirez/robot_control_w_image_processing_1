86ec3f5ed2528084b79a05b1a0e284bd63acfc7a
Fortunato <33538533+fortunatoramirez@users.noreply.github.com>
2022-07-15 17:46:49 -0700

    Update Tx_image_procesing_1.py

--- Tx_image_procesing_1.py
+++ Tx_image_procesing_1.py
@@ -3,45 +3,42 @@ import mediapipe as mp
 import time
 from socketIO_client import SocketIO
 
+# IP Server (String number)
+CONNECT_TO_SERVER = False # True / False
+SERVER_IP = 'localhost' # localhost | i.p.ser.ver
 
-# MOTOR = 1
-# INIT_POS = 90
-# MIN_POS = 0
-# MAX_POS = 180
-# MASC = 0
+MOTOR = 1
+MIN_POS = 0
+MAX_POS = 180
+MASC = 0
 
 # MOTOR = 2
-# INIT_POS = 90
 # MIN_POS = 50
 # MAX_POS = 180
 # MASC = 1000
 
 # MOTOR = 3
-# INIT_POS = 50
 # MIN_POS = 0
 # MAX_POS = 180
 # MASC = 2000
 
-MOTOR = 4
-INIT_POS = 120
-MIN_POS = 0
-MAX_POS = 180
-MASC = 3000
+# MOTOR = 4
+# MIN_POS = 0
+# MAX_POS = 180
+# MASC = 3000
 
 # MOTOR = 5
-# INIT_POS = 90
 # MIN_POS = 80
-# MAX_POS = 100
+# MAX_POS = 180
 # MASC = 4000
 
+INIT_POSITIONS = [90,90,50,120,100]
+
 # Steps [Degrees]
-STEP_MIN = 0.2 # For low velocity
-STEP_MAX = 1   # For high velocity
+STEP_MIN = 1 # For low velocity
+STEP_MAX = 5   # For high velocity
 
-# IP Server (String number)
-SERVER_IP = 'i.p.ser.ver'
 
-CONNECT_TO_SERVER = False # True/False
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -57,7 +54,6 @@ controlling_hand_message = ""
 controlling_message = "Sin control"
 controlling_message_color = (0,0,0)
 
-M1 = INIT_POS
 LOW = False
 HIGH = False
 
@@ -144,6 +140,7 @@ while True:
                     M1R_SIM_TEXT_X =  M1R_X1 + round(R_W/5)
                     M1R_SIM_TEXT_Y =  M1R_Y1 - round(R_W/2)
 
+
         elif left_detected and not right_detected:
             controlling_hand_message = "IZQUIERDA"
             # mp_drawing.draw_landmarks(frame, left_hand_landmarks, mp_hands.HAND_CONNECTIONS)
@@ -181,22 +178,22 @@ while True:
             # Check if TIP is Over or Under the PIP line
             # (slower)
             if RIGHT_INDEX_FINGER_TIP_Y <= RIGHT_INDEX_FINGER_PIP_Y and RIGHT_INDEX_FINGER_PIP_Y > height/2:
-                if M1-STEP_MIN >= MIN_POS:
-                    M1 = M1-STEP_MIN
+                if INIT_POSITIONS[MOTOR-1] >= MIN_POS:
+                    INIT_POSITIONS[MOTOR-1] = INIT_POSITIONS[MOTOR-1]-STEP_MIN
                 cv2.rectangle(frame, ( M1L_X1 , M1L_Y1 ) , ( M1L_X2 , M1L_Y2 ),(0,255,0), -1)
             elif RIGHT_PINKY_FINGER_TIP_Y <= RIGHT_PINKY_FINGER_PIP_Y and RIGHT_PINKY_FINGER_PIP_Y > height/2:
-                if M1+STEP_MIN <= MAX_POS:
-                    M1 = M1+STEP_MIN
+                if INIT_POSITIONS[MOTOR-1] <= MAX_POS:
+                    INIT_POSITIONS[MOTOR-1] = INIT_POSITIONS[MOTOR-1]+STEP_MIN
                 cv2.rectangle(frame, ( M1R_X1 , M1R_Y1 ) , ( M1R_X2 , M1R_Y2 ),(0,255,0), -1)
             
             # (faster)
             if RIGHT_INDEX_FINGER_TIP_Y <= RIGHT_INDEX_FINGER_PIP_Y and RIGHT_INDEX_FINGER_PIP_Y <= height/2:
-                if M1-STEP_MAX >= MIN_POS:
-                    M1 = M1-STEP_MAX
+                if INIT_POSITIONS[MOTOR-1] >= MIN_POS:
+                    INIT_POSITIONS[MOTOR-1] = INIT_POSITIONS[MOTOR-1]-STEP_MAX
                 cv2.rectangle(frame, ( M1L_X1 , M1L_Y1 ) , ( M1L_X2 , M1L_Y2 ),(0,255,0), -1)
             elif RIGHT_PINKY_FINGER_TIP_Y <= RIGHT_PINKY_FINGER_PIP_Y and RIGHT_PINKY_FINGER_PIP_Y <= height/2:
-                if M1+STEP_MAX <= MAX_POS:
-                    M1 = M1+STEP_MAX
+                if INIT_POSITIONS[MOTOR-1]+STEP_MAX <= MAX_POS:
+                    INIT_POSITIONS[MOTOR-1] = INIT_POSITIONS[MOTOR-1]+STEP_MAX
                 cv2.rectangle(frame, ( M1R_X1 , M1R_Y1 ) , ( M1R_X2 , M1R_Y2 ),(0,255,0), -1)
             
 
@@ -216,13 +213,65 @@ while True:
             # Center line
             # cv2.line(frame, (0,round(height/2)), (width,round(height/2)), (0,0,0), 2)
 
-            cv2.putText(frame, "POS = {}".format(round(M1)), (M1L_X1,M1_TEXT_Y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+            cv2.putText(frame, "POS = {}".format(round(INIT_POSITIONS[MOTOR-1])), (M1L_X1,M1_TEXT_Y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
             cv2.putText(frame, "VELOCIDAD = "+velocity_message, (M1L_X1-50,M1_TEXT_Y-50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, velocity_message_color, 2)
-
-            # Send angle to server
-            # socketIO.emit("nuevo_mensaje",str((M1+MASC))+'\n')
-            # print(str(M1))
         
+        if controlling and left_detected:
+            LEFT_INDEX_FINGER_TIP_X = round(left_hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * width)
+            LEFT_INDEX_FINGER_TIP_Y = round(left_hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * height)
+            cv2.circle(frame, (LEFT_INDEX_FINGER_TIP_X,LEFT_INDEX_FINGER_TIP_Y),5,(0,0,255),6)
+
+            cv2.rectangle(frame, (50, INFO_TEXT_Y+200 ) , ( 75 , INFO_TEXT_Y+200+25 ),(0,0,255), -1)
+            cv2.rectangle(frame, (50, INFO_TEXT_Y+250 ) , ( 75 , INFO_TEXT_Y+200+75 ),(0,0,255), -1)
+            cv2.rectangle(frame, (50, INFO_TEXT_Y+300 ) , ( 75 , INFO_TEXT_Y+200+125 ),(0,0,255), -1)
+            cv2.rectangle(frame, (50, INFO_TEXT_Y+350 ) , ( 75 , INFO_TEXT_Y+200+175 ),(0,0,255), -1)
+            cv2.rectangle(frame, (50, INFO_TEXT_Y+400 ) , ( 75 , INFO_TEXT_Y+200+225 ),(0,0,255), -1)
+
+            cv2.putText(frame, "M5", (5,INFO_TEXT_Y+220), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 1)
+            cv2.putText(frame, "M4", (5,INFO_TEXT_Y+270), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 1)
+            cv2.putText(frame, "M3", (5,INFO_TEXT_Y+320), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 1)
+            cv2.putText(frame, "M2", (5,INFO_TEXT_Y+370), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 1)
+            cv2.putText(frame, "M1", (5,INFO_TEXT_Y+420), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 1)
+
+            if LEFT_INDEX_FINGER_TIP_X > 50 and  LEFT_INDEX_FINGER_TIP_Y > INFO_TEXT_Y+200 and LEFT_INDEX_FINGER_TIP_X < 75 and LEFT_INDEX_FINGER_TIP_Y < INFO_TEXT_Y+200+25:
+                MOTOR = 5
+                MIN_POS = 80
+                MAX_POS = 180
+                MASC = 4000
+                cv2.rectangle(frame, (50, INFO_TEXT_Y+200 ) , ( 75 , INFO_TEXT_Y+200+25 ),(0,255,0), -1)
+            
+            elif LEFT_INDEX_FINGER_TIP_X > 50 and  LEFT_INDEX_FINGER_TIP_Y > INFO_TEXT_Y+250 and LEFT_INDEX_FINGER_TIP_X < 75 and LEFT_INDEX_FINGER_TIP_Y < INFO_TEXT_Y+200+75:
+                MOTOR = 4
+                MIN_POS = 0
+                MAX_POS = 180
+                MASC = 3000
+                cv2.rectangle(frame, (50, INFO_TEXT_Y+250 ) , ( 75 , INFO_TEXT_Y+200+75 ),(0,255,0), -1)
+            elif LEFT_INDEX_FINGER_TIP_X > 50 and  LEFT_INDEX_FINGER_TIP_Y > INFO_TEXT_Y+300 and LEFT_INDEX_FINGER_TIP_X < 75 and LEFT_INDEX_FINGER_TIP_Y < INFO_TEXT_Y+200+125:
+                MOTOR = 3
+                MIN_POS = 0
+                MAX_POS = 180
+                MASC = 2000
+                cv2.rectangle(frame, (50, INFO_TEXT_Y+300 ) , ( 75 , INFO_TEXT_Y+200+125 ),(0,255,0), -1)
+            
+            elif LEFT_INDEX_FINGER_TIP_X > 50 and  LEFT_INDEX_FINGER_TIP_Y > INFO_TEXT_Y+350 and LEFT_INDEX_FINGER_TIP_X < 75 and LEFT_INDEX_FINGER_TIP_Y < INFO_TEXT_Y+200+175:
+                MOTOR = 2
+                MIN_POS = 50
+                MAX_POS = 180
+                MASC = 1000
+                cv2.rectangle(frame, (50, INFO_TEXT_Y+350 ) , ( 75 , INFO_TEXT_Y+200+175 ),(0,255,0), -1)
+            
+            elif LEFT_INDEX_FINGER_TIP_X > 50 and  LEFT_INDEX_FINGER_TIP_Y > INFO_TEXT_Y+400 and LEFT_INDEX_FINGER_TIP_X < 75 and LEFT_INDEX_FINGER_TIP_Y < INFO_TEXT_Y+200+225:
+                MOTOR = 1
+                MIN_POS = 0
+                MAX_POS = 180
+                MASC = 0
+                cv2.rectangle(frame, (50, INFO_TEXT_Y+400 ) , ( 75 , INFO_TEXT_Y+200+225 ),(0,255,0), -1)
+            
+        if controlling and CONNECT_TO_SERVER:
+            # Send angle to server
+            socketIO.emit("nuevo_mensaje",str((INIT_POSITIONS[MOTOR-1]+MASC))+'\n')
+            # print(str(M1))
+
         right_detected = False
         left_detected = False
 
@@ -243,5 +292,8 @@ while True:
         break
 
 
+
 cap.release()
 cv2.destroyAllWindows()
+
+        
